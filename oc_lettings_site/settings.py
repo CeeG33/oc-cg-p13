583a2b390212f8e2e54b52e55805e502eae76387
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-11-02 10:26:47 +0100

    Sentry configuration completed

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -21,7 +21,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'sentry': {
-            'level': 'ERROR',
+            'level': 'WARNING',
             'class': 'oc_lettings_site.sentry_logging.SentryHandler',
         },
     },
