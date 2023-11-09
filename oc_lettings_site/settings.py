07f163b1b8f2b39cfc1cc6011ad1500bf9157bc8
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-11-09 16:49:41 +0100

    Bug fix to app secret key

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -39,7 +39,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("APP_SECRET_KEY")
+APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
+SECRET_KEY = APP_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
