de39b1ab8d031e79e38b149156fa46e9b0fad6be
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-11-10 12:56:32 +0100

    custom_500 bug fix test

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -36,7 +36,7 @@ def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
 
-def custom_500(request, exception):
+def custom_500(request):
     """
     Render the 500 error page.
 
