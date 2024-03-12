f55dbf369bdea657d9432e08d1f57edaf2d39ed8
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2024-03-12 20:21:03 +0100

    Sentry configuration simplified

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,22 +15,6 @@ sentry_sdk.init(
 )
 
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'sentry': {
-#             'level': 'ERROR',
-#             'class': 'oc_lettings_site.sentry_logging.SentryHandler',
-#         },
-#     },
-#     'root': {
-#         'level': 'ERROR',
-#         'handlers': ['sentry'],
-#     },
-# }
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
